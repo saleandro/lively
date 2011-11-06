811d0e1f7c48c1bc0a27265a09bbe2db9a87af8d
Sabrina Leandro <sabrina@songkick.com>
2011-11-06 13:10:29 -0500

    Fixing redis data store

--- lib/api_access.rb
+++ lib/api_access.rb
@@ -3,37 +3,38 @@ require 'open-uri'
 require 'json'
 
 module DataStore
-
-  def self.set(key, value)
-    if @redisdb
-      @redisdb[key] = value
-    else
-      if key = self.get(key)
-        @sqldb[:cache].filter(:key => key).update(:value => value)
+  class << self
+    def set(key, value)
+      if store.is_a?(Redis)
+        store[key] = value
       else
-        @sqldb[:cache].insert(:key => key, :value => value)
+        if key = get(key)
+          store[:cache].filter(:key => key).update(:value => value)
+        else
+          store[:cache].insert(:key => key, :value => value)
+        end
       end
     end
-  end
 
-  def self.get(key)
-    if @redisdb
-      @redisdb.get(key)
-    else
-      @sqldb[:cache].filter(:key => key).select(:value).single_value
-    end
-  end
-
-  private
-
-  def self.store
-    if ENV["DATABASE_URL"]
-      @sqldb = Sequel.connect ENV["DATABASE_URL"]
-    else
-      uri = URI.parse('redis://localhost:6789')
-      @redisdb ||= Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :user => uri.user, :thread_safe => true)
+    def get(key)
+      if store.is_a?(Redis)
+        store.get(key)
+      else
+        store[:cache].filter(:key => key).select(:value).single_value
+      end
     end
 
+    private
+
+    def store
+      if ENV["DATABASE_URL"]
+        @sqldb ||= Sequel.connect ENV["DATABASE_URL"]
+      else
+        uri = URI.parse('redis://localhost:6379')
+        @redisdb ||= Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :user => uri.user, :thread_safe => true)
+      end
+
+    end
   end
 end
 
