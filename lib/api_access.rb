ffdc9373e33ece5b24edb7331f73c3017ee15e62
Sabrina Leandro <sabrina@songkick.com>
2011-11-06 12:08:08 -0500

    Redis on heroku

--- lib/api_access.rb
+++ lib/api_access.rb
@@ -4,7 +4,13 @@ require 'json'
 
 module DataStore
   def self.store
-    @db ||= Redis.new(:thread_safe => true)
+    if ENV["REDISTOGO_URL"]
+      uri = URI.parse(ENV["REDISTOGO_URL"])
+    else
+      uri = URI.parse('redis://localhost:6789')
+    end
+
+    @db ||= Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
   end
 end
 
